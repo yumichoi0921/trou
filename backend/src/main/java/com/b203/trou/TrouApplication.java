1786e529b719f3970c69c2cabbaf48dab1d7e72c
an7188 <an92818@gmail.com>
2022-03-18 11:50:06 +0900

    init: 프로젝트생성

---
+++ backend/src/main/java/com/b203/trou/TrouApplication.java
@@ -0,0 +1,12 @@
+package com.b203.trou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrouApplication.class, args);
+	}
+}
