7cdc5bfd656ed3cf5616e89e161e7de70548a7a6
Noah <23338687+enjay27@users.noreply.github.com>
2022-03-21 15:57:19 +0900

    build: 설정 파일 Database 비밀번호 분리

--- backend/src/main/java/com/b203/trou/TrouApplication.java
+++ backend/src/main/java/com/b203/trou/TrouApplication.java
@@ -2,13 +2,21 @@ package com.b203.trou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class TrouApplication {
 
+	private static final String PROPERTIES =
+			"spring.config.location="
+					+"classpath:/application.yml"
+					+",classpath:/application-secret.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(TrouApplication.class, args);
+		new SpringApplicationBuilder(TrouApplication.class)
+				.properties(PROPERTIES)
+				.run();
 	}
 }
