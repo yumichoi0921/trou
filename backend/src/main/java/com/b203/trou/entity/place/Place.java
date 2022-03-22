939418630af8e8a31f73879a0586a289584f22d8
LeeYena <cathy192@naver.com>
2022-03-22 11:05:11 +0900

    chore: place column 수정

--- backend/src/main/java/com/b203/trou/entity/place/Place.java
+++ backend/src/main/java/com/b203/trou/entity/place/Place.java
@@ -17,14 +17,24 @@ public class Place extends BaseEntity {
     @GeneratedValue
     @Column(name = "placeId")
     private Long id;
+    private String addr1;
+    private String adde2;
+    private String areaCode;
+    private String cat3;
+    private String contentTypeId;
+    private String firstImage;
+    private String firstImage2;
+    private BigDecimal mapX;
+    private BigDecimal mapY;
 
+    private int mLevel;
+    private int readCount;
+    private String sigunguCode;
+    private String tel;
     private String placeName;
-
-    private String placeInfo;
-
-    private BigDecimal longitude;
-
-    private BigDecimal latitude;
+    private String eventStartDate;
+    private String eventEndDate;
+    private String overview;
 
     @OneToMany(mappedBy = "place")
     private List<Review> reviews = new ArrayList<>();
