0e447321bb809f3c579ec2f7fe0ca4b6b6b2ce15
Noah <23338687+enjay27@users.noreply.github.com>
2022-03-24 16:27:30 +0900

    fix: 잘못 생성한 Repository 삭제

--- backend/src/main/java/com/b203/trou/repository/review/ScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.b203.trou.repository.review;
-
-import com.b203.trou.entity.review.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-}
