79d0e210630e7e64eb5d3184d0a0de5674cb7353
LeeYena <cathy192@naver.com>
2022-03-24 13:52:07 +0900

    feat: Add bootstrap

--- frontend/src/component/Main.js
+++ frontend/src/component/Main.js
@@ -1,23 +1,35 @@
 import styles from "./Main.module.css";
-
-export default function Main(){
-
-
-return(
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Row, Col, Container } from "react-bootstrap";
+export default function Main() {
+  return (
     <div>
-        <div className={styles.searchDiv}>
-            <h2>어디로 떠나시나요?</h2>
-            <input className={styles.searchBar} type="text" />
-        </div>
-        <div>
-            <h3>이 여행은 어떠신가요??</h3>
-            <div>
-
-            </div>
-        </div>
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">
+          Button #1
+        </Button>
+        <Button as={Col} variant="secondary" className="mx-2">
+          Button #2
+        </Button>
+        <Button as={Col} variant="success">
+          Button #3
+        </Button>
+      </Row>
+      <Container>
+        <Row>
+          <Col>어디로 떠나시나요</Col>
+        </Row>
+        <Row></Row>
+        <Row></Row>
+      </Container>
+      <div className={styles.searchDiv}>
+        <h2>어디로 떠나시나요?</h2>
+        <input className={styles.searchBar} type="text" />
+      </div>
+      <div>
+        <h3>이 여행은 어떠신가요??</h3>
+        <div></div>
+      </div>
     </div>
-
-
-);
-
+  );
 }
