79f2fe9f75533d45e1ab66a50e36bd646afdb422
yumichoi <yumichoi0921@gmail.com>
2022-03-25 14:56:12 +0900

    init: frontend init

--- data/getApiData.py
+++ data/getApiData.py
@@ -92,7 +92,7 @@ def getEvent(df):
         condition = df['place_id'] == place_id
         df.loc[condition, 'event_start_date'] = event_start_date
         df.loc[condition, 'event_end_date'] = event_end_date
-    insertPlace(df)
+    # insertPlace(df)
 
 
 getPlace()
