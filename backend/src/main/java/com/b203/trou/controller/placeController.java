9eb45729b765d47ef1a6d9ceb4c27edc5224ec2a
LeeYena <cathy192@naver.com>
2022-03-25 14:32:15 +0900

    feat: main화면 프론트 제작

---
+++ backend/src/main/java/com/b203/trou/controller/placeController.java
@@ -0,0 +1,23 @@
+package com.b203.trou.controller;
+
+import com.b203.trou.entity.place.Place;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+@RestController
+public class placeController {
+
+    @GetMapping("/place")
+    public ResponseEntity<?> selectPlaces(String input ){
+
+
+
+
+
+    }
+
+
+}
