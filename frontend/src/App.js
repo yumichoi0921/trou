3ec5deff5c65717bcd98cc79d37cbdd43af37c63
LeeYena <cathy192@naver.com>
2022-03-25 14:22:27 +0900

    feat: place contoller 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Styles from "./App.module.css";
 import Main from "./component/Main";
 import Ip from "./component/Ip";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import CustomAxios from './CustomAxios';
 
@@ -14,6 +10,7 @@ function App() {
     // <p> 이 기기의 ip 주소는 {ip}입니다.</p>
     <BrowserRouter>
       <Routes>
+        <Route path="/"></Route>
         <Route path="/ip" element={<Ip />}></Route>
         <Route path="/main" element={<Main />} />
       </Routes>
