eceeae45cc3d7f30db741ad356776c9f9f2268ac
yumichoi <yumichoi0921@gmail.com>
2022-03-25 18:20:04 +0900

    feat: 계획페이지:여행지 확인페이지 수정

--- frontend/src/components/plan/check.js
+++ frontend/src/components/plan/check.js
@@ -5,18 +5,22 @@ import { Grid, Box } from "@mui/material";
 const Area = styled(Box)({
   minHeight: 600,
   backgroundColor: "#90caf9",
+  padding: 25,
   "&:hover": {
     backgroundColor: "#64b5f6",
     opacity: [0.9, 0.8, 0.7],
   },
-  padding: 25,
 });
 
 const PlaceItem = styled(Box)({
-  backgroundColor: "#bdbdbd",
+  backgroundColor: "#e0e0e0",
   borderRadius: 16,
   padding: 15,
   margin: 15,
+  "&:hover": {
+    backgroundColor: "#bdbdbd",
+    opacity: [0.9, 0.8, 0.7],
+  },
 });
 
 const check = () => {
@@ -26,6 +30,7 @@ const check = () => {
         <Grid item md={3}>
           <Area>
             <div>날짜</div>
+
             <PlaceItem>여행지1</PlaceItem>
             <PlaceItem>여행지1</PlaceItem>
             <PlaceItem>여행지1</PlaceItem>
