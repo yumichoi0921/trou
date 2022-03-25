5ec4836831f0257a5906d6093fb62ee05859622a
LeeYena <cathy192@naver.com>
2022-03-25 17:51:18 +0900

    feat: make placeDto

---
+++ backend/src/main/java/com/b203/trou/model/place/ReviewDto.java
@@ -0,0 +1,16 @@
+package com.b203.trou.model.place;
+
+import com.b203.trou.entity.review.Score;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ReviewDto {
+
+    String userName;
+    String content;
+    Score score;
+
+
+
+
+}
