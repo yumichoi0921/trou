dfc536b2b02234cfa35171fb2f4242f79b8724bc
yumichoi <yumichoi0921@gmail.com>
2022-03-25 16:15:06 +0900

    feat: material-ui 추가

---
+++ frontend/src/components/plan/check.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "@mui/material/Button";
+const check = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default check;
