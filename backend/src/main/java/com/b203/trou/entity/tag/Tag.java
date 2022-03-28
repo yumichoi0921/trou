1bbc7025000b8882a1e28055740714503d328e68
yumichoi <yumichoi0921@gmail.com>
2022-03-28 12:07:38 +0900

    feat: TagController 생성

--- backend/src/main/java/com/b203/trou/entity/tag/Tag.java
+++ backend/src/main/java/com/b203/trou/entity/tag/Tag.java
@@ -1,10 +1,12 @@
 package com.b203.trou.entity.tag;
 
 import com.b203.trou.entity.BaseEntity;
+import lombok.Getter;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 public class Tag extends BaseEntity {
 
     @Id
