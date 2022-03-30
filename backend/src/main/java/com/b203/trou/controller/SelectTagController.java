168b08129351f3ce5b6c11c9521e7f4b9eb05d04
an7188 <an92818@gmail.com>
2022-03-30 11:25:34 +0900

    feat: 태그 선택 controller 생성

---
+++ backend/src/main/java/com/b203/trou/controller/SelectTagController.java
@@ -0,0 +1,15 @@
+package com.b203.trou.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/tag")
+public class SelectTagController {
+
+
+
+
+}
--- backend/src/main/java/com/b203/trou/controller/TagController.java
+++ backend/src/main/java/com/b203/trou/controller/TagController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +21,16 @@ public class TagController {
 
     private final TagService tagService;
 
-//    @GetMapping
-//    public ResponseEntity<?> getAllTags() {
-//        try {
-//            List<TagDto> tags = tagService.getTags();
-//            return ResponseEntity.status(200).body(tags);
-//        } catch (IllegalStateException e) {
-//            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
-//        }
-//    }
+    @GetMapping
+    public ResponseEntity<?> getAllTags() {
+        try {
+            List<TagDto> tags = tagService.getTags();
+            return ResponseEntity.status(200).body(tags);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
+        }
+    }
+
+
+
 }
