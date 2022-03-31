fe148c63124f4ee3fa9545a0b477bacef2fbd58a
김지수 <luky0628@naver.com>
2022-03-31 17:45:12 +0900

    여행 세부일정 수정

--- frontend/src/components/myPage/TripDetail.js
+++ frontend/src/components/myPage/TripDetail.js
@@ -5,6 +5,7 @@ import { Fragment } from 'react';
 import Collapse from '@mui/material/Collapse';
 import Place from './Place';
 import Schedule from "./Schedule";
+import Review from "./Review";
 
 
 const TripDetail = () => {
@@ -20,70 +21,80 @@ const TripDetail = () => {
           routeId: 1,
           planId: '부산여행',
           place: '부산역',
-          order: 1
+          order: 1,
+          review: 5
         },
         {
           id: 2,
           routeId: 1,
           planId: '부산여행',
           place: '수변공원',
-          order: 2
+          order: 2,
+          review: 4
         },
         {
           id: 3,
           routeId: 1,
           planId: '부산여행',
           place: '자갈치시장',
-          order: 3
+          order: 3,
+          review: 3
         },
         {
           id: 4,
           routeId: 1,
           planId: '부산여행',
           place: '해운대',
-          order: 4
+          order: 4,
+          review: 2
         },
         {
           id: 5,
           routeId: 1,
           planId: '부산여행',
           place: '숙소',
-          order: 5
+          order: 5,
+          review: 1
         },
         {
           id: 6,
           routeId: 2,
           planId: '부산여행',
           place: '남천동',
-          order: 1
+          order: 1,
+          review: 3,
         },
         {
           id: 7,
           routeId: 2,
           planId: '부산여행',
           place: '남천동',
-          order: 2
+          order: 2,
+          review: 2,
         },
         {
           id: 8,
           routeId: 2,
           planId: '부산여행',
           place: '남천동',
-          order: 3
+          order: 3,
+          review: 5,
         },
         {
           id: 9,
           routeId: 2,
           planId: '부산여행',
           place: '남천동',
-          order: 4
+          order: 4,
+          review: 4,
         },
         {
           id: 10,
           routeId: 2,
           planId: '부산여행',
           place: '남천동',
-          order: 5
+          order: 5,
+          review: 3,
         },
     ];
     const routes = [
@@ -96,6 +107,7 @@ const TripDetail = () => {
             planId: 1
         }
     ];
+
     return(
         <Fragment>
             <Grid container spacing={2}>
@@ -117,7 +129,10 @@ const TripDetail = () => {
                 
                 <Schedule></Schedule>
                 {orders.map((order)=>(
+                  <div>
                     <Place key={order.id} order={order} />
+                    <Review key={order.id} score={order.review}></Review>
+                  </div>
                 ))}
             </Grid>                       
         </Fragment>
