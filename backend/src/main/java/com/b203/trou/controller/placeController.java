d47a000a534c7f5f2907c025462a0412b62471b8
김중재 <rlawndwo1305@naver.com>
2022-04-01 18:25:37 +0900

    feat: 세부계획페이지 구현중

--- backend/src/main/java/com/b203/trou/controller/placeController.java
+++ backend/src/main/java/com/b203/trou/controller/placeController.java
@@ -15,11 +15,15 @@ public class placeController {
 
    private final PlaceService placeService;
 
+   @GetMapping("/detail/{placeId}")
+   public ResponseEntity<?> selectPlaces(@PathVariable long placeId ) {
+       PlaceDto place = placeService.getPlace(placeId);
+       return ResponseEntity.ok(place);
+   }
+
    @GetMapping("/{keyword}")
     public ResponseEntity<?> selectPlaces(@PathVariable("keyword") String keyword ){
-
          List<PlaceDto> places = placeService.getPlaces(keyword);
-
 //
 //       ReviewDto review1 = new ReviewDto("userName", "너무 좋아요", Score.FOUR);
 //       ReviewDto review2 = new ReviewDto("userName", "너무 좋아요", Score.FOUR);
@@ -29,10 +33,7 @@ public class placeController {
 //       PlaceDto place2 = new PlaceDto(List.of(review1, review2, review3), "한라산", "http://tong.visitkorea.or.kr/cms/resource/83/1070183_image2_1.jpg");
 //       PlaceDto place3 = new PlaceDto(List.of(review1, review2, review3), "한라산", "http://tong.visitkorea.or.kr/cms/resource/83/1070183_image2_1.jpg");
 //       List<PlaceDto> places = new ArrayList<>(List.of(place,place2,place3));
-
        return ResponseEntity.ok(places);
-
-
    }
 
 
