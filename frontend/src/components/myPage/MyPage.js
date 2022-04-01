6808c53c3daada8c4aafca7895273d216f2d024d
김지수 <luky0628@naver.com>
2022-04-01 19:02:11 +0900

    마이페이지 수정중

--- frontend/src/components/myPage/MyPage.js
+++ frontend/src/components/myPage/MyPage.js
@@ -2,18 +2,39 @@ import Plan from './Plan';
 import Button from "@mui/material/Button";
 import styles from './MyPage.module.css';
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-const MyPage = async () => {
+
+const MyPage = () => {
+    const [plans, setPlans] = useState([]);
     const userId = '1';
-    const objects = await axios.get("/plan/" + userId);
-    const plans = objects.data;
-    console.log(plans);
+    //const result = await axios.get("/plan/" + userId);
+    
+    //console.log(result.data)
+    //setPlans([...plans, result.data])
+    useEffect(() => {
+        async function getTags() {
+          //const res = await axios.get("http://localhost:8080/tag");
+
+          const result = await axios.get("/plan/" + userId);
+          const temp = result.data;
+          console.log(temp);
+          setPlans(
+            //result.data
+            temp
+          );
+          //console.log(plans)
+        }
+        getTags();
+        //console.log(plans);
+      });
 
     return(
         <div>
             <Button className={styles.button} variant="text">삭제</Button>
-            {plans.map((plan)=>(
-                <Plan key={plan.planId} plan={plan} />
+            {plans.map((plan, index)=>(
+                // <Plan key={plan.planId} startDate={plan.startDate}/>
+                {index}
             ))}
         </div>
     )
