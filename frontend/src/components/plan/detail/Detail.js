c769631e18a4c0ece943e21c7c66988243a2e302
김중재 <rlawndwo1305@naver.com>
2022-04-01 18:57:34 +0900

    feat: 세부계획페이지 구현중

--- frontend/src/components/plan/detail/Detail.js
+++ frontend/src/components/plan/detail/Detail.js
@@ -5,41 +5,15 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { styled, Grid, Box, Button, Stack, Paper, Divider, Menu, MenuItem, alpha, TextareaAutosize, Avatar, InputLabel, FormControl, Select } from "@mui/material";
 import { Link } from "react-router-dom";
-import BackImg from "../../../imgs/sun.png"
 import SelectDay from "./SelectDay"
-import ShareModal from "./ShareModal"
 import axios from "axios";
 import OrderListBar from "./OrderListBar";
+import Memo from "./Memo";
+import ModifyMemo from "./ModifyMemo";
+import Weather from "./Weather";
+import ShareFriends from "./ShareFriends";
 
 
-function stringToColor(string) {
-    let hash = 0;
-    let i;
-    
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#';
-
-    for (i = 0; i < 3; i += 1) {
-        const value = (hash >> (i * 8)) & 0xff;
-        color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-    
-    return color;
-}
-
-function stringAvatar(name) {
-    return {
-        sx: {
-            bgcolor: stringToColor(name),
-        },
-        children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-    };
-}
 const Area = styled(Box)({
     minHeight: '100%',
     backgroundColor: "#90caf9",
@@ -81,87 +55,7 @@ const KakaoMap = () => {
     );
 };
 
-// 공유된 친구 이름 props로 받아서 넣기                    
-const ShareFriends = () => {
-    return (
-        <Stack direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={1} >
-            <p>초대 친구들</p>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <Avatar {...stringAvatar('Kent Dodds')} />
-                </Grid>
-                <Grid item xs={6}>
-                    <Avatar {...stringAvatar('Jed Watson')} />
-                </Grid>
-                <Grid item xs={6}>
-                    <Avatar {...stringAvatar('Tim Neutkens')} />
-                </Grid>
-            </Grid>
-            <ShareModal></ShareModal>
-            {/* <Button variant="outlined">공유</Button> */}
 
-        </Stack>
-    );
-};
-
-const handleChange = (event) => {
-    setMemo(event.target.value);
-};
-
-const ModifyMemo = ({memo, setMemoState, setMemo}) => {
-    return (
-        <form>
-            <Stack direction="column"
-                justifyContent="center"
-                alignItems="center"
-                spacing={1} >
-                <p>메모장</p>
-                <textarea value={memo} onChange={(e) => handleChange(e)}></textarea>
-                {/* <TextareaAutosize
-                    aria-label="minimum height"
-                    minRows={10}
-                    style={{ width: '90%' }}
-                /> */}
-                <Button variant="contained" onClick={event => {
-                    event.preventDefault();
-                    setMemoState(true);
-                }}>완료</Button>
-            </Stack>
-        </form>
-    );
-};
-
-const Memo = ({memo, setMemoState}) => {
-    return (
-        <Stack direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={1} >
-            <p>메모장</p>
-            <p>{memo}</p>
-            <Button variant="outlined" onClick={event=>{
-                event.preventDefault();
-                setMemoState(false);
-            }}>수정</Button>
-        </Stack>
-    );
-};
-
-const Weather = () => {
-    return (
-        <Stack direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={1} >
-            <p>날씨</p>
-            <Avatar src={BackImg} />
-            {/* <Avatar src="C:\SSAFY\workspace\work-trou\frontend\src\imgs\sun.png" /> */}
-        </Stack>
-    );
-};
 
 const Detail = () => {
     const [day, setDay] = React.useState(0);
--- frontend/src/components/plan/detail/OrderListBar.js
+++ frontend/src/components/plan/detail/OrderListBar.js
@@ -3,17 +3,31 @@ import axios from "axios";
 
 
 const OrderListBar = ({index, order}) => {
-    const name = '';
+    // let name = '';
 
-    const getPlace = async () => {
-
-    };
-    // order.placeId
-    useEffect(() => {
-        getPlace();
-    }, []);
+    // const getPlace = async () => {
+    //     try{
+    //         const res = await axios({
+    //             method: "get",
+    //             url: `/place/detail/${order.placeId}`,    
+    //             baseURL: "http://localhost:8080",
+    //             timeout: 2000,
+    //         });
+    //         name = res.data.placeName;
+    //     } catch{
+    //         console.log('에러발생');
+    //     }
+    // };
+    // // order.placeId
+    // useEffect(() => {
+    //     getPlace();
+    // }, []);
     return (
-        <p>{index}번째 {name}</p>
+        <div>
+            {index}번째 장소 : {order.placeName} <br/>
+            startTime :  {order.startTime}  <br/>
+            endTime :  {order.endTime}
+        </div>
     );
 };
 
