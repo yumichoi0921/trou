4232666166396d8b945855469bfc97b378b4313d
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-01 11:25:39 +0900

    feat: 계획 페이지 Step1

--- frontend/src/components/plan/Plan.js
+++ frontend/src/components/plan/Plan.js
@@ -4,7 +4,8 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Date from "./Date";
-import PersonToggleButton from "./PersonToggleButton";
+import TagToggleButton from "./TagToggleButton";
+import { Button, Stack } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,12 +16,31 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function FullWidthGrid() {
+  const [tags, setTags] = useState([]);
+  const [curPage, setPage] = useState(1);
+
+  function nextPage() {
+    if (curPage === 3) return;
+    setPage(curPage + 1);
+  }
+  function prevPage() {
+    if (curPage === 1) return;
+    setPage(curPage - 1);
+  }
   return (
     <Fragment>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           {/* Grid 오른쪽 부분 */}
           <Grid item xs={6} md={4}>
+            <Grid container spacing={2}>
+              <Grid item xs={6} md={5}>
+                여행지
+              </Grid>
+              <Grid item xs={6} md={7}>
+                <Button>부산</Button>
+              </Grid>
+            </Grid>
             <Grid container spacing={2}>
               <Grid item xs={6} md={5}>
                 가는 날
@@ -38,7 +58,22 @@ export default function FullWidthGrid() {
               </Grid>
             </Grid>
             <Item>태그 선택</Item>
-            <PersonToggleButton></PersonToggleButton>
+            <TagToggleButton tags={tags} setTags={setTags}></TagToggleButton>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={2}
+              mt={3}
+              mr={3}
+            >
+              {curPage !== 1 ? (
+                <Button onClick={prevPage}>이전</Button>
+              ) : (
+                <Box></Box>
+              )}
+              <Button onClick={nextPage}>다음</Button>
+            </Stack>
           </Grid>
 
           {/* Grid 왼쪽 시작 */}
