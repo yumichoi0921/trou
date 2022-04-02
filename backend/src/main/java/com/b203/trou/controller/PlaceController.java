ce5e3451d0483b77a459e330773f56597307d331
LeeYena <cathy192@naver.com>
2022-04-02 21:32:54 +0900

    feat: Python api 받아오기

--- backend/src/main/java/com/b203/trou/controller/PlaceController.java
+++ backend/src/main/java/com/b203/trou/controller/PlaceController.java
@@ -1,12 +1,38 @@
 package com.b203.trou.controller;
 import com.b203.trou.model.place.PlaceDto;
+import com.b203.trou.model.place.PlaceRequestDto;
+import com.b203.trou.model.place.PlaceResponseDto;
+import com.b203.trou.model.user.UserHistoryDto;
 import com.b203.trou.service.place.PlaceService;
+import com.b203.trou.service.user.UserHistoryService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/place")
@@ -14,7 +40,7 @@ import java.util.List;
 public class PlaceController {
 
    private final PlaceService placeService;
-
+   private final UserHistoryService userHistoryService;
    @GetMapping("/{keyword}")
     public ResponseEntity<?> selectPlaces(@PathVariable("keyword") String keyword ){
 
@@ -24,13 +50,38 @@ public class PlaceController {
 
 
    }
-//
-//   @GetMapping("/recommand/{userId}")
-//    public ResponseEntity<?>recommandPlaces(@PathVariable("userId") String userId){
-//
-//        List<PlaceDto> userHistoryPlaces = placeService.getPlacesByUserId(userId);
-//
-//   }
+
+   @GetMapping("/recommand/{userId}")
+    public ResponseEntity<?>recommandPlaces(@PathVariable("userId") long userId) throws IOException {
+       RestTemplate restTemplate = new RestTemplate();
+        List<UserHistoryDto> userHistoryPlaces = placeService.getHistory(userId);
+        String baseUrl = "http://127.0.0.1:8000/recommand/";
+
+       HttpHeaders headers    = new HttpHeaders();
+       headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+       HttpEntity request = new HttpEntity(headers);
+
+       List<PlaceRequestDto> places = new ArrayList<>();
+
+       for (UserHistoryDto h: userHistoryPlaces) {
+           String placeName = placeService.getPlacesById(h.getPlace().getId());
+           places.add(new PlaceRequestDto(placeName));
+       }
+
+
+       PlaceResponseDto[] res=restTemplate.postForObject(baseUrl,places, PlaceResponseDto[].class);
+
+       for(int i=0; i < res.length; i++) {
+           System.out.println("======== res : " + i + " ========");
+           System.out.println(res[i].getPlace_id() + " " + res[i].getPlace_name() + " " + res[i].getTags());
+       }
+
+
+
+
+       return ResponseEntity.ok(res);
+   }
+
 
 
 
--- backend/src/main/java/com/b203/trou/service/place/PlaceService.java
+++ backend/src/main/java/com/b203/trou/service/place/PlaceService.java
@@ -1,15 +1,23 @@
 package com.b203.trou.service.place;
 
+import com.b203.trou.entity.place.Place;
 import com.b203.trou.entity.tag.PlaceTag;
 import com.b203.trou.entity.tag.Tag;
+import com.b203.trou.entity.user.User;
+import com.b203.trou.entity.user.UserHistory;
 import com.b203.trou.model.place.PlaceDto;
+import com.b203.trou.model.user.UserDto;
+import com.b203.trou.model.user.UserHistoryDto;
 import com.b203.trou.repository.place.PlaceRepository;
 import com.b203.trou.repository.tag.PlaceTagRepository;
 import com.b203.trou.repository.tag.TagRepository;
+import com.b203.trou.repository.user.UserHistoryRepository;
+import com.b203.trou.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,6 +29,8 @@ public class PlaceService {
     private final PlaceRepository placeRepository;
     private final TagRepository tagRepository;
     private final PlaceTagRepository placeTagRepository;
+    private final UserHistoryRepository userHistoryRepository;
+    private final UserRepository userRepository;
     public List<PlaceDto> getPlaces(String keyword) { // 여행지의 태그, 여행지의 이름
 
         List<Tag> tags = tagRepository.findByTagNameLike(keyword);
@@ -47,8 +57,23 @@ public class PlaceService {
 
 
 
+    public List<UserHistoryDto> getHistory(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException("해당하는 유저가 없습니다."));
+
+
+        List<UserHistory> userHistory = userHistoryRepository.findByUser(user);
+        System.out.println("userHistory: "+userHistory);
+        List<UserHistoryDto> userHistoryDtoList = userHistory.stream().map(u -> new UserHistoryDto(u)).collect(Collectors.toList());
+        return userHistoryDtoList;
 
     }
+
+    public String getPlacesById(long placeId) {
+
+         Place place = placeRepository.findById(placeId).orElseThrow(()->new IllegalArgumentException("해당하는 장소가 없습니다."));
+        return place.getPlaceName();
+    }
+}
 
 
     
