eadbc62e674c64c9b777720859fa978c002ab18d
an7188 <an92818@gmail.com>
2022-04-04 14:25:14 +0900

    feat: TockenDto 생성

---
+++ backend/src/main/java/com/b203/trou/model/user/TockenDto.java
@@ -0,0 +1,13 @@
+package com.b203.trou.model.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TockenDto {
+
+    private String tocken;
+}
