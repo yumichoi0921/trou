c074f4c4808abb886b2b8833bef927803454cb09
LeeYena <cathy192@naver.com>
2022-04-04 17:58:27 +0900

    feat: 없는 이미지 랜덤 출력

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -2,17 +2,28 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import react, { useState, useEffect } from "react";
 import backImg from "../../imgs/back.png";
+
+import parse from "html-react-parser";
 import {
   CommitSharp,
   LineAxisOutlined,
   PlayCircleSharp,
   Search,
+  Star,
 } from "@mui/icons-material";
 import styles from "./Main.css";
 
 import axios from "axios";
 //import  Button  from "@mui/material/Button";
-import { Grid, styled, Box, TextField, Modal, Typography } from "@mui/material";
+import {
+  Grid,
+  styled,
+  Box,
+  TextField,
+  Modal,
+  Typography,
+  Rating,
+} from "@mui/material";
 import { height } from "@mui/system";
 export default function Main() {
   const [show, setShow] = useState(false);
@@ -26,10 +37,10 @@ export default function Main() {
   const handleShow = () => setShow(true);
   const classes = styles;
   function clickSite(e) {
+    console.log(e);
     //e.preventDefault();
-    console.log(e.place_id);
     setImgName(e.placeName);
-    setSrc(e.placeImage);
+    setSrc(e.image);
     setReviews(e.reviews);
     handleShow();
   }
@@ -47,9 +58,15 @@ export default function Main() {
 
   function Reivew(props) {
     return (
-      <Box>
-        <h1>{props.user}</h1> <p>{props.content}</p>
-      </Box>
+      <li style={{ margin: 10, textAlign: "center", my: 3 }}>
+        <b style={{ marginRight: 20 }}>{props.user}</b> {props.content}
+        <Rating
+          style={{ marginLeft: 20 }}
+          name="read-only"
+          value={props.score}
+          readOnly
+        />
+      </li>
     );
   }
   const handleKeyPress = (e) => {
@@ -62,30 +79,56 @@ export default function Main() {
     let keyword = e;
     console.log(keyword);
     let array = [];
+    let index = 0;
     const response = await axios.get("/place/" + keyword).then((r) => {
       array = r.data.map((place, i) => {
-        if (place.image == null) {
-          place.image = backImg;
+        console.log(place);
+        place.isImg = true;
+        if (place.image === null) {
+          index = Math.floor(Math.random() * (5 - 1) + 1);
+          console.log(index);
+          place.image = `/imgs/img${index}.jpg`;
+          place.index = index;
+          place.isImg = false;
+        } else {
+          console.log(place.img);
+
+          place.isImg = true;
         }
         return (
           <div
-            style={{ float: "left", marginLeft: 20, height: 400, width: 400 }}
+            style={{ float: "left", marginLeft: 20, height: 300, width: 300 }}
             key={i}
           >
-            <h2> {place.placeName}</h2>
-
-            <img
-              onClick={() => {
-                clickSite(place);
-              }}
-              src={place.image}
-              alt={place.placeId}
-              name={place.placeName}
-              style={{
-                height: "300px",
-                width: "400px",
-              }}
-            />
+            <h3> {place.placeName}</h3>
+            {place.isImg && (
+              <img
+                onClick={() => {
+                  clickSite(place);
+                }}
+                src={place.image}
+                alt={place.placeId}
+                name={place.placeName}
+                style={{
+                  height: "200px",
+                  width: "300px",
+                }}
+              />
+            )}
+            {!place.isImg && (
+              <img
+                onClick={() => {
+                  clickSite(place);
+                }}
+                src={`/imgs/img${place.index}.jpg`}
+                alt={place.placeId}
+                name={place.placeName}
+                style={{
+                  height: "200px",
+                  width: "300px",
+                }}
+              />
+            )}
           </div>
         );
       });
@@ -96,33 +139,77 @@ export default function Main() {
   useEffect(() => {
     setUserId(1);
     const userId = 1;
+
     const response = axios
       .get("/place/recommand/" + userId)
       .then((r) => {
         console.log(r.data);
         let array = [];
+        let index = 0;
         array = r.data.map((place, i) => {
-          if (place.img === " ") place.img = backImg;
+          place.isImg = true;
+          if (place.img === " ") {
+            index = Math.floor(Math.random() * (5 - 1) + 1);
+            console.log(index);
+            place.img = index;
+            place.isImg = false;
+            place.index = index;
+            place.img = `/imgs/img${index}.jpg`;
+          } else {
+            console.log(place.img);
 
+            place.isImg = true;
+          }
+          console.log(place.isImg);
           return (
             <div
-              style={{ float: "left", marginLeft: 20, height: 400, width: 400 }}
+              style={{ float: "left", marginLeft: 20, height: 300, width: 300 }}
               key={i}
             >
-              <h2> {place.place_name}</h2>
-
-              <img
-                onClick={() => {
-                  clickRecommand(place);
-                }}
-                src={place.img}
-                alt={place.place_id}
-                name={place.place_name}
-                style={{
-                  height: "300px",
-                  width: "400px",
-                }}
-              />
+              <h3> {place.place_name}</h3>
+              {place.isImg}
+              {place.isImg && (
+                <img
+                  onClick={() => {
+                    clickRecommand(place);
+                  }}
+                  src={place.img}
+                  alt={place.place_id}
+                  name={place.place_name}
+                  style={{
+                    height: "200px",
+                    width: "300px",
+                  }}
+                /> /* {/*  */ //}
+              )}
+              {!place.isImg && (
+                <img
+                  onClick={() => {
+                    clickRecommand(place);
+                  }}
+                  src={`/imgs/img${place.index}.jpg`}
+                  alt={place.place_id}
+                  name={place.place_name}
+                  style={{
+                    height: "200px",
+                    width: "300px",
+                  }}
+                /> /* {/*  */ //}
+              )}
+              {/* /* : (
+                <img
+                  onClick={() => {
+                    clickRecommand(place);
+                  }}
+                  src={require(`../../imgs/${index}.jpg`)}
+                  alt={place.place_id}
+                  name={place.place_name}
+                  style={{
+                    height: "200px",
+                    width: "300px",
+                  }}
+                />
+              )} */}{" "}
             </div>
           );
         });
@@ -131,19 +218,13 @@ export default function Main() {
       .catch((e) => {
         console.log(e);
       });
-
-    // let array = [];
-    // array = response.data.map((place, i) => {
-    //   console.log(place.place_Id + "" + i);
-    //   //return();
-    // });
   }, []);
 
   const SearchDiv = styled(Grid)({
     marginTop: 50,
     paddingTop: 50,
-    marginLeft: 100,
-    marginRight: 100,
+    // marginLeft: 100,
+    // marginRight: 100,
     paddingBottom: 80,
     backgroundImage: `url(${backImg})`,
     // width: "100%",
@@ -187,8 +268,6 @@ export default function Main() {
               id="outlined-required"
               label="도시 또는 태그 입력"
               onKeyPress={(e) => handleKeyPress(e)}
-
-              // onChange={(e) => SearchPlace(e.target.value)}
             />
           </Box>
         </SearchDiv>
@@ -201,25 +280,50 @@ export default function Main() {
         </Box>
       </Grid>
 
-      <Box style={{ marginLeft: 200 }}>{places}</Box>
+      <Box style={{ marginLeft: "15%" }}>{places}</Box>
       <Modal
         open={show}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {imgName}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <img src={src} alt="" style={{ width: "500px" }}></img>
+        <Grid
+          container
+          sx={style}
+          xs={12}
+          // style={{ float: "left" }}
+          spacing={3}
+        >
+          <Grid item xs={12}>
+            <h2> {imgName} </h2>
+          </Grid>
+          <Grid item container>
+            <Grid item>
+              <img src={src} alt="" style={{ width: "400px" }}></img>
+            </Grid>
 
-            {reviews.map((r, i) => (
-              <Reivew key={i} user={r.userName} content={r.content}></Reivew>
-            ))}
-          </Typography>
-        </Box>
+            <Grid
+              item
+              style={{ textAlign: "center", marginLeft: 90 }}
+              sx={{ justifyContent: "center", my: 3 }}
+            >
+              {" "}
+              <h4>사용자 후기</h4>
+              <ul style={{ listStyle: "none" }}>
+                {reviews.map((r, i) => (
+                  <Reivew
+                    style={{ textAlign: "center" }}
+                    sx={{ justifyContent: "center", my: 3 }}
+                    key={i}
+                    user={r.userName}
+                    content={r.content}
+                    score={r.score}
+                  ></Reivew>
+                ))}
+              </ul>
+            </Grid>
+          </Grid>
+        </Grid>
       </Modal>
     </Grid>
   );
