ac4396ba688c8cd1980b7b3f5cf34fe8d955c703
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-04 14:45:33 +0900

    chore: 불필요 파일 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import About from "./components/About";
 import Check from "./components/plan/step3/Check";
 import MyPage from "./components/myPage/MyPage";
 import TripDetail from "./components/myPage/TripDetail";
@@ -12,12 +11,8 @@ import Main from "./components/main/Main";
 import Login from "./components/FirstMain/Login";
 import Join from "./components/FirstMain/Join";
 import Detail from "./components/plan/detail/Detail";
-
 import Plan from "./components/plan/Plan";
-import PlanStep1 from "./components/plan/step1/PlanStep1";
 const App = () => {
-  const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
   const plans = [
     {
       id: 1,
@@ -69,90 +64,6 @@ const App = () => {
     },
   ];
 
-  // useEffect(() => {
-  //   const getTasks = async () => {
-  //     const tasksFromServer = await fetchTasks();
-  //     setTasks(tasksFromServer);
-  //   };
-
-  //   getTasks();
-  // }, []);
-
-  // // Fetch Tasks
-  // const fetchTasks = async () => {
-  //   const res = await fetch("http://localhost:5000/tasks");
-  //   const data = await res.json();
-
-  //   return data;
-  // };
-
-  // // Fetch Task
-  // const fetchTask = async (id) => {
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`);
-  //   const data = await res.json();
-
-  //   return data;
-  // };
-
-  // // Add Task
-  // const addTask = async (task) => {
-  //   const res = await fetch("http://localhost:5000/tasks", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(task),
-  //   });
-
-  //   const data = await res.json();
-
-  //   setTasks([...tasks, data]);
-
-  //   // const id = Math.floor(Math.random() * 10000) + 1
-  //   // const newTask = { id, ...task }
-  //   // setTasks([...tasks, newTask])
-  // };
-
-  // // Delete Task
-  // const deleteTask = async (id) => {
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-  //     method: "DELETE",
-  //   });
-  //   //We should control the response status to decide if we will change the state or not.
-  //   res.status === 200
-  //     ? setTasks(tasks.filter((task) => task.id !== id))
-  //     : alert("Error Deleting This Task");
-  // };
-
-  // // Toggle Reminder
-  // const toggleReminder = async (id) => {
-  //   const taskToToggle = await fetchTask(id);
-  //   const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
-
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-  //     method: "PUT",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(updTask),
-  //   });
-
-  // const data = await res.json();
-
-  // setTasks(
-  //   tasks.map((task) =>
-  //     task.id === id ? { ...task, reminder: data.reminder } : task
-  //   )
-  // );
-  // };
-
-  const getPlanDetail = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
-    const data = await res.json();
-
-    return data;
-  };
-
   return (
     <Router>
       <Routes>
@@ -165,15 +76,10 @@ const App = () => {
       <div className="container">
         <Routes>
           <Route path="/main" element={<Main />} />
-          <Route path="/about" element={<About />} />
           <Route path="/mypage/" element={<MyPage />} />
           <Route path="/tripDetail" element={<TripDetail />} />
           <Route path="/check" element={<Check />} />
-          <Route path="/plan" element={<Plan />}>
-            <Route path="step1" element={<PlanStep1 />}></Route>
-            <Route path="step2" element={<PlanStep1 />}></Route>
-            <Route path="step3" element={<PlanStep1 />}></Route>
-          </Route>
+          <Route path="/plan/*" element={<Plan />} />
           <Route path="/planDetail" element={<Detail />} />
         </Routes>
         {/* <Footer /> */}
