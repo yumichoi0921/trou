5c36f56a4f400fec5fa43f06d993574774214915
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-04 14:45:56 +0900

    feat: Route 이용하여 Plan Step 페이지 분리

--- frontend/src/components/plan/Plan.js
+++ frontend/src/components/plan/Plan.js
@@ -7,7 +7,8 @@ import PlanStep1 from "./step1/PlanStep1";
 import PlanStep2 from "./step2/PlanStep2";
 import KakaoMap from "./step3/Step3KakaoMap";
 import PlanRecommendation from "./step2/PlaceRecommendation";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Router, Routes, useNavigate } from "react-router-dom";
+import Check from "./step3/Check";
 
 export default function Plan() {
   const [tags, setTags] = useState([]);
@@ -15,12 +16,33 @@ export default function Plan() {
   const [beginDate, setBeginDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [plans, setPlans] = useState([]);
-
   return (
     <Fragment>
+      <nav>
+        <Link to="step1">step1</Link>
+        <br></br>
+        <Link to="step2">step2</Link>
+        <br></br>
+        <Link to="step3">step3</Link>
+      </nav>
+
       <Routes>
         <Route
-          exact
+          path="*"
+          element={
+            <PlanStep1
+              tags={tags}
+              setTags={setTags}
+              curPage={curPage}
+              setPage={setPage}
+              beginDate={beginDate}
+              setBeginDate={setBeginDate}
+              endDate={endDate}
+              setEndDate={setEndDate}
+            ></PlanStep1>
+          }
+        ></Route>
+        <Route
           path="step1"
           element={
             <PlanStep1
@@ -35,6 +57,8 @@ export default function Plan() {
             ></PlanStep1>
           }
         ></Route>
+        <Route path="step2" element={<h1>step2</h1>}></Route>
+        <Route path="step3" element={<h1>step3</h1>}></Route>
       </Routes>
     </Fragment>
   );
