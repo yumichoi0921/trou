a43abc3b28f593c3e2cf04fb11f3c72d4f4d4226
an7188 <an92818@gmail.com>
2022-04-04 14:25:48 +0900

    feat: SecurityConfig 접근 가능한 구간 설정

--- backend/src/main/java/com/b203/trou/config/SecurityConfig.java
+++ backend/src/main/java/com/b203/trou/config/SecurityConfig.java
@@ -1,27 +1,58 @@
 package com.b203.trou.config;
 
-import org.springframework.http.HttpMethod;
+import com.b203.trou.jwt.JwtAccessDeniedHandler;
+import com.b203.trou.jwt.JwtAuthenticationEntryPoint;
+import com.b203.trou.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //web보안 활성화
 @EnableWebSecurity
+//@PreAuthorize 어노테이션을 메소드 단위로 추가하기 위함.
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//         http.csrf().disable()
-//                .authorizeRequests() //예외처리 기능
-//                 .and()
-//                 .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-//                .antMatchers(HttpMethod.OPTIONS, "/api/*").permitAll() //OPTIONS 메소드 허락 - preflight 요청에 대한 접근 허용
-//                .antMatchers("/api/users/signup","/api/users/signin").permitAll() // 가입 및 인증 주소는 누구나 접근가능
-//        // .antMatchers(HttpMethod.GET, "//refresh-access-token").permitAll() // 로그인이 유지중이라면 토큰이 만료되는 시점에 리프레쉬 해주는 요청. 추후 기능 추가 시 주석 해제
-////			    .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능 배포시 주석 해제!
-//                 .and()
-//                 .anyRequest().authenticated(); //나머지 요청은 모두 인증이 되어야 접근 가능
+        //Token 방식을 사용하므로 csrf 설정을 disable
+         http.csrf().disable()
+                 // 만들어줬던 주입받는 코드
+                 .exceptionHandling()
+                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                 .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+
+                 .and()
+                 .authorizeRequests()
+                 .antMatchers("/users/*").permitAll()
+                 .anyRequest().authenticated()
+
+                 .and()
+                 .apply(new JwtSecurityConfig(tokenProvider)); // 커스텀하게 작선한 jwtSecurityConfig도 적용
+
 
     }
 }
